2271c6db8c1d12cd0f566cb21103caef47ebef22
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-28 17:14:54 -0300

    implemented routes

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
-import { MyNotes } from "../pages/MyNotes";
-import { Perfil } from "../pages/Perfil";
+import { Notes } from "../pages/Notes";
+import { NotFound } from "../pages/NotFound";
+import { Profile } from "../pages/Profile";
 import { Sign } from "../pages/Sign";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign" element={<Sign />} />
-      <Route path="/perfil" element={<Perfil />} />
-      <Route path="/notes" element={<MyNotes />} />
+      <Route path="" element={<Home />} />
+      <Route path="sign" element={<Sign />} />
+      <Route path="note/:id" element={<Notes />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
