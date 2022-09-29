fb508d0a1672bfc6f9a2ae8e1a18b954f1db4bf2
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-29 01:09:31 -0300

    implemented check if is authenticated

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 import { Home } from "../pages/Home";
 import { Notes } from "../pages/Notes";
 import { NotFound } from "../pages/NotFound";
@@ -6,12 +8,21 @@ import { Profile } from "../pages/Profile";
 import { Sign } from "../pages/Sign";
 
 export function Router() {
+  const { authenticated } = useContext(AuthContext);
+  console.log(authenticated);
   return (
     <Routes>
       <Route path="" element={<Home />} />
       <Route path="sign" element={<Sign />} />
-      <Route path="note/:id" element={<Notes />} />
-      <Route path="profile" element={<Profile />} />
+      {authenticated ? (
+        <>
+          <Route path="note/:id" element={<Notes />} />
+          <Route path="profile" element={<Profile />} />
+        </>
+      ) : (
+        ""
+        // <Route path="/" element={<Home />} />
+      )}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
