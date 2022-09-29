ab6d480531b29aeb5596c009ae4a04d737fd86ea
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-29 15:29:04 -0300

    alter url to prod

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const url = "https://post-it.up.railway.app";
-
-console.log(import.meta.env.VITE_URL);
+const url = import.meta.env.VITE_URL;
 
 export const apiClient = axios.create({
   baseURL: url,
