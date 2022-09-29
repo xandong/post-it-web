4c51f6b8ba5efb48a8528ae36ce5b1e6d81380ec
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-29 01:09:55 -0300

    restore

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,4 +1,9 @@
 import axios from "axios";
 
-const url = import.meta.env.BASE_URL && "http://localhost:3333";
-export default axios.create({ baseURL: url });
+const url = import.meta.env.VITE_URL || "http://localhost:3333";
+
+console.log(import.meta.env.VITE_URL);
+
+export default axios.create({
+  baseURL: url,
+});
