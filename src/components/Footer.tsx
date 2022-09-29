c2bf8069db07e90ee137d8c9a61ee938294a30e0
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-29 15:20:23 -0300

    Create comopenent Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className="p-6 text-center">
+      Desenvolvido por{" "}
+      <a href="https://github.com/xandong" target="_blank" className="a">
+        xandong
+      </a>
+    </footer>
+  );
+}
